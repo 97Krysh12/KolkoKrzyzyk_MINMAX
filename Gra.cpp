9c85b80776f6c244c00305acfec17031c137cc9a
97Krysh12 <37145346+97Krysh12@users.noreply.github.com>
2018-06-10 01:13:32 +0200

    Update Gra.cpp

--- Gra.cpp
+++ Gra.cpp
@@ -41,7 +41,12 @@ Gra::Gra(char gracz)
 
 Gra::~Gra()
 {
-	delete plansza; 
+	for (int i = 0; i < size; i++)
+	{
+		delete plansza[i];
+
+	}
+	delete []plansza; 
 }
 
 void Gra::setSize(int size)
@@ -100,13 +105,13 @@ int Gra::wygrana(char **plansza, char gracz, int size) // dla gracza 'X' zwraca 
 
 	 // Sprawdzamy wiersze
 
-	
-	while (i != size +1 && j != size +1) {
+
+	while (i != size || j != size) {
 		for (i = licznik_W; i < zRzedu + licznik_W; i++) {
 
 			for (j = licznik_K; j < zRzedu + licznik_K; j++) {
 
-				if (plansza[i][j] != gracz) { j = zRzedu + licznik_K -1;  test = false; break; } // j = zRzedu + licznik_K;
+				if (plansza[i][j] != gracz) { j = zRzedu + licznik_K; test = false; break; } //  j = zRzedu + licznik_K -1;
 
 				else test = true;
 			}
@@ -118,58 +123,85 @@ int Gra::wygrana(char **plansza, char gracz, int size) // dla gracza 'X' zwraca 
 			else if (test == true && gracz == 'O') return -10;
 		}
 		licznik_W++;
-		if (i == size ) {
+		if (i == size) {
 			licznik_W = 0;
-			licznik_K ++;
+			licznik_K++;
+		}
+
+	}
+	licznik_K = 0;
+	licznik_W = 0;
+	i = 0;
+	j = 0;
+
+	// Sprawdzamy kolumny  
+	while (i != size || j != size) {
+		for (i = 0; i < size; i++) { // (zRzedu + licznik_W)
+
+			for (j = licznik_K; j < zRzedu + licznik_K; j++) { // (zRzedu + licznik_K)
+				if (plansza[j][i] != gracz) { j = zRzedu + licznik_K;  test = false; break; } //  j = zRzedu + licznik_K -1;
+				else test = true;
+			}
+			if (test == true && gracz == 'X') return 10;
+			else if (test == true && gracz == 'O') return -10;
+
+		}
+
+		licznik_K++;
+	}
+
+	// }
+	licznik_K = 0;
+	licznik_W = 0;
+	i = 0;
+	j = 0;
+	// Sprawdzamy przekątną lewo-prawo
+	while (i != size || j != size) {
+		j = licznik_W;
+		for (i = licznik_K; i < zRzedu + licznik_K; i++) {
+
+			if (plansza[j][i] != gracz) { i = zRzedu + licznik_K; j = licznik_W + zRzedu; test = false; break; }
+			else test = true;
+			j++;
+
+		}
+		if (test == true && gracz == 'X') return 10;
+		else if (test == true && gracz == 'O') return -10;
+
+		licznik_K++;
+		if (i == size) {
+			licznik_W++;
+			licznik_K = 0;
+		}
+
+	}
+	licznik_K = 0;
+	licznik_W = 0;
+	i = 0;
+	j = 0;
+
+	// Sprawdzamy przekątną prawo- lewo
+	while (i != -1 || j != size) {
+		j = licznik_W;
+		for (i = size - 1 - licznik_K; i > size - 1 - licznik_K - zRzedu; i--) {
+			 
+			if (plansza[j][i] != gracz) { i = size - 1 - licznik_K - zRzedu; j = licznik_W + zRzedu ; test = false; break; }
+			else test = true;
+			j++;
+		}
+		if (test == true && gracz == 'X') return 10;
+		else if (test == true && gracz == 'O') return -10;
+
+		licznik_K++;
+		if (i == -1) {
+			licznik_W++;
+			licznik_K = 0;
 		}
 		
 	}
-	 licznik_K = 0;
-	 licznik_W = 0;
-	 i = 0;
-	 j = 0;
-
-	 // Sprawdzamy kolumny  
-	 while (i != size  && j != size) {
-		 for (i = licznik_W; i < (zRzedu + licznik_W); i++) {
-
-			 for (j = licznik_K; j < (zRzedu + licznik_K); j++) {
-				 if (plansza[j][i] != gracz) { j = zRzedu + licznik_K -1;  test = false; break; }
-				 else test = true;
-			 }
-			 if (test == true && gracz == 'X') return 10;
-			 else if (test == true && gracz == 'O') return -10;
-
-		 }
-
-		 licznik_W++;
-		 if (i == size ) {
-			 licznik_W = 0;
-			 licznik_K++;
-		 }
-
-	 }
-
-	// Sprawdzamy przekątną lewo-prawo
-	for (i = 0; i < size; i++) {
-		if (plansza[i][i] != gracz) { test = false; break; }
-		else test = true;
-	}
-	if (test == true && gracz == 'X') return 10;
-	else if (test == true && gracz == 'O') return -10;
-
-
-
-	// Sprawdzamy przekątną prawo- lewo
-	for (i = size - 1; i >= 0; i--) {
-
-		if (plansza[i][i] != gracz) { test = false; break; }
-		else test = true;
-	}
-	if (test == true && gracz == 'X') return 10;
-	else if (test == true && gracz == 'O') return -10;
-	
 	if (!isMove(plansza, size)) return 0;
+	else if (gracz == 'X') return 5;
+	else return -5;
 }
 
 bool Gra::isMove(char ** plansza, int size)
@@ -186,12 +218,8 @@ void Gra::makeRuch(char **plansza, int wiersz, int kolumna)
 {
 	if (!isMove(plansza, size)) return;
 	plansza[wiersz][kolumna] = this->gracz;
-	int score = wygrana(plansza, this->gracz, size);
-	Show(size);
-	if ( score == 10) cout << "koniec gry" << endl << "WYGRAl GRACZ 'X'";
-	else if(score == -10) cout << "koniec gry" << endl << "WYGRAl GRACZ 'O'";
 	setLicznik(getLicznik() + 1);
-	if (this->licznik == size*size) cout << "koniec gry" << endl << "REMIS!!! '" << this->gracz << "'";
+	if(isKoniec(plansza)) return;
 	this->gracz = (this->gracz == 'O') ? 'X' : 'O';
 	setGracz(this->gracz);
 }
@@ -200,10 +228,16 @@ void Gra::ruchCzlowiek(char ** plansza, int size)
 {
 	int RuchX, RuchY;
 	
-	cout << "Kolej na ruch gracza " << this->gracz << endl;
-	cout << "Podaj wiersz oraz kolumne ruchu (W,K) : ";
-	cin >> RuchY >> RuchX;
-	//plansza[RuchY][RuchX] = gracz;
+	
+	do {
+		cout << "Kolej na ruch gracza " << this->gracz << endl;
+		cout << "Podaj wiersz oraz kolumne ruchu (W,K) : ";
+		cin >> RuchY >> RuchX;
+		if (plansza[RuchY][RuchX] != '-') cout << "Pole juz Zajete. Sprobuj ponownie \n";
+	} while (plansza[RuchY][RuchX] != '-');
+	
+
+	
 
 	makeRuch(plansza, RuchY, RuchX);
 	
@@ -238,52 +272,72 @@ void Gra::Ustawienia()
 	this->zRzedu = zRzedu;
 }
 
-int Gra::minmax(char ** plansza, int depth, char gracz, int bestValueMax, int bestValueMin)
+int Gra::minmax(char ** plansza, int depth, char gracz, int alfa, int beta)
 {
 	//TODO zamien bool wygrana na int wygrana, zeby tu mozna bylo sprawdzac czy koniec gry dla wartosci ruchow +10/-10
 	int value = 0;
 	int score = wygrana(plansza, gracz, this->size);
 
-	if (score == 10) return score;
-	if (score == -10) return score;
-	if(score == 0) return score;
-	if (depth == 0) return score;
+	if (score == 10) return score - depth;
+	if (score == -10) return score + depth;
 
+	if (gracz == 'X' && score == 0) return score + depth;
+	if (gracz == 'O' && score == 0) return score - depth;
+
+	if (gracz == 'X' && depth == 5) return score ; 
+	if (gracz == 'O' && depth == 5) return score;
+	
+	 //if maximizer
 	if (gracz == 'X') {
-
+		
 		for (int i = 0; i < this->size; i++) {
 			for (int j = 0; j < this->size; j++) {
 				if (plansza[i][j] == '-') {
 					plansza[i][j] = 'X';
-					value = minmax(plansza, depth - 1, 'O', bestValueMax, bestValueMin);
-					bestValueMax = max(bestValueMax, value);
+					value = minmax(plansza, depth + 1, 'O', alfa, beta);
+					if(value > alfa) alfa = value;
 					plansza[i][j] = '-';
-					if (bestValueMin <= bestValueMax) break;
+					if (alfa >= beta) return alfa;
 				}
+				
 			}
+			if (alfa >= beta) return alfa;
 		}
-		return bestValueMax;
+		return alfa;
 	}
+	// if minimizer
 	else if (gracz == 'O') {
-
+		
 		for (int i = 0; i < size; i++) {
 			for (int j = 0; j < size; j++) {
 				if (plansza[i][j] == '-') {
 					plansza[i][j] = 'O';
-					value = minmax(plansza, depth - 1, 'X', bestValueMax, bestValueMin);
-					bestValueMin = min(bestValueMin, value);
+					value = minmax(plansza, depth + 1, 'X', alfa, beta);
+					if (value < beta) beta = value;
 					plansza[i][j] = '-';
-					if(bestValueMin <= bestValueMax) break;
+					
 				}
+				
 			}
+			if (alfa >= beta) return beta;
 		}
-		return bestValueMin;
+		return beta;
 	}
+}
+
+bool Gra::isKoniec(char ** plansza)
+{
+	int score = wygrana(plansza, this->gracz, size);
+	Show(size);
+	if (score == 10) { cout << "koniec gry" << endl << "WYGRAl GRACZ 'X'"; return 1; }
+	else if (score == -10) { cout << "koniec gry" << endl << "WYGRAl GRACZ 'O'"; return 1; }
+	else if (this->licznik == size * size) cout << "koniec gry" << endl << "REMIS!!! ";
+	else return 0;
 }
 
 Pole Gra::RuchPC(char ** plansza, int size)
 {
-	int bestValue = 1000;
+	int bestValue = 10000;
 	Pole bestMove;
 	bestMove.wiersz = -1;
 	bestMove.kolumna = -1;
@@ -292,7 +346,7 @@ Pole Gra::RuchPC(char ** plansza, int size)
 		for (int j = 0; j < size; j++) {
 			if (plansza[i][j] == '-') {
 				plansza[i][j] = this->gracz;
-				int value = minmax(plansza, size * size, 'X', -100, 100);
+				int value = minmax(plansza, 0, 'X', -1000, 1000);
 				plansza[i][j] = '-';
 				if (value < bestValue) {
 					bestValue = value;
